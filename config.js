158c26e2789a7c5e633074ed9c2f3395cf9b5bd0
Nam Nguyen <goo.itpro@gmail.com>
2022-01-17 23:21:53 +0700

    express-generator init project

---
+++ config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+  BCRYPT_SECRET: process.env.BCRYPT_SECRET,
+};
---
+++ controllers/user-controller.js
@@ -0,0 +1,12 @@
+exports.login = () => {
+  throw new Error('Not implemented!');
+};
+exports.logout = () => {
+  throw new Error('Not implemented!');
+};
+exports.register = () => {
+  throw new Error('Not implemented!');
+};
+exports.activate = () => {
+  throw new Error('Not implemented!');
+};
