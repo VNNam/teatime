808d2f1cd1fb653263fc2684d1710471e5ba6144
Nam Nguyen <goo.itpro@gmail.com>
2022-01-19 01:29:26 +0700

    exports groups, usergroups, message

--- database/index.js
+++ database/index.js
@@ -2,6 +2,9 @@ const { Message, UserGroup, User, Group } = require('./schema');
 const { DB_CONNECTION, DB_NAME } = require('../config');
 const mongoose = require('mongoose');
 const users = require('./user');
+const groups = require('./group');
+const usergroups = require('./user-group');
+const messages = require('./message');
 
 /**
  * Tao ket noi den mongodb
@@ -13,9 +16,8 @@ function teatimedb() {
   return mongoose
     .connect(DB_CONNECTION, { dbName: DB_NAME })
     .then((db) => {
-      return { users };
+      return { users, groups, usergroups, messages };
     })
     .catch((error) => console.log(error));
 }
-teatimedb();
 module.exports = { teatimedb };
