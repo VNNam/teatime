7176769b6654baabc6be69828f28ffb4f9a6be9c
Lương Khắc Nam <namlk@hamsa.co>
2022-02-07 20:30:35 +0700

    change structure of test folder

--- __test__/routers/index.test.js
+++
@@ -1,7 +0,0 @@
-const request = require('supertest');
-const app = require('../../app');
-
-test('Get / should return a status 200', async () => {
-  const response = await request(app).get('/');
-  expect(response.statusCode).toBe(200);
-});
